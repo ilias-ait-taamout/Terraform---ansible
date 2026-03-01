e87e13795ff2495e0354c97d2a0356bc54d40f6a
Ubuntu <ubuntu@ip-172-31-93-96.ec2.internal>
2026-03-01 03:50:12 +0000

    first two steps in terraform config

---
+++ main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web-security-group"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_instance" "web" {
+  ami                    = "ami-0b6c6ebed2801a5cb"
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "Terraform-Web-Server"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "key_name" {
+  description = "SSH key pair name"
+  type        = string
+}
+
+
